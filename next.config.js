1be3e7d6a7f8664cd10e8cf7477b188694c74857
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-12-04 11:47:28 +0100

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
+const withAntdLess = require("next-plugin-antd-less");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com']
-  }
-}
+    domains: ["res.cloudinary.com"],
+  },
+  
+};
 
-module.exports = nextConfig
+module.exports = () => {
+  const plugins = [withAntdLess];
+  return plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  });
+};
