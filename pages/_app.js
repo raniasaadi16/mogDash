9bd4804ffa88030e53b454ad255a2cf780f184d8
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2022-12-04 11:52:00 +0100

    edit app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
+import 'antd/dist/reset.css';
 import { Router } from 'next/router'
 import { useState } from 'react'
 import { AppContextProvider } from '../components/context/AppContext'
 import Loading from '../components/layouts/global/Loading'
 import Layout from '../components/layouts/Layout'
 import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }) {
   const [loading, setloading] = useState(false)
   Router.events.on('routeChangeStart', () => {
